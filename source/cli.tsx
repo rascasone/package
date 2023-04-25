16dba5348c20e5f92fd6a86c37f6d5031c9381f1
Jakub Jurník <kubis.jurnik@gmail.com>
2023-04-25 17:37:36 +0200

    init

---
+++ source/cli.tsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ rsc-pkg
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ rsc-pkg --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
