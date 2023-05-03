5ea369b813aa126f2e35a385a86c6270daceb9d0
Jakub Jurník <kubis.jurnik@gmail.com>
2023-05-03 16:11:19 +0200

    added husky

--- source/app.tsx
+++ source/app.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Text } from 'ink';
+import {Text} from 'ink';
 
 type Props = {
 	template: string | undefined;
 	packageName: string | undefined;
 };
 
-export default function App({ template = 'plain Typescript', packageName }: Props) {
+export default function App({
+	template = 'plain Typescript',
+	packageName,
+}: Props) {
 	return (
 		<>
 			<Text>
-				Hello, it looks like you want to create package named <Text color="green">{packageName}</Text> using <Text color="green">{template}</Text>
+				Hello, it looks like you want to create package named{' '}
+				<Text color="green">{packageName}</Text> using{' '}
+				<Text color="green">{template}</Text>
 			</Text>
-			<Text>
-				Good luck with that!
-			</Text>
+			<Text>Good luck with that!</Text>
 		</>
 	);
 }
